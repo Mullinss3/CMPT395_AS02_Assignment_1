0ee2776c88cf60bfbd7cfcde65bf56813950f6b7
Mullinss3 <mullinss3@mymacewan.ca>
2023-01-31 08:38:42 -0700

    Completed Requirement 2 of Kata 3, Modified Tests
    - password_Validator function can now validate if a password is too short and if it has at least 2 digits in it.
    - Modified tests to test for success in both number of characters and digits, failure in just number of digits, and failure in both number of characters and digits

--- CMPT395_Assignment_1_Kata_3.py
+++ CMPT395_Assignment_1_Kata_3.py
@@ -45,10 +45,24 @@ def password_Validator(password):
               True if the string passes the checks, False if it does not, and
               any failure messages if it failed a check
     '''    
+    # setting up variables
+    password_validation, password_error = True, None
+    # checking if the password is at least 8 characters long
     if (len(password) < 8):
-        return False, "Password must be at least 8 characters"
-    else:
-        return True, "None"
+        password_validation, password_error = False, "Password must be at least 8 characters"
+    # checking if the password has at least  two numbers
+    count = 0
+    for character in password:
+        if (character.isnumeric()):
+            count += 1
+    if (count < 2):
+        # password already had another validation error
+        if (password_validation == False):
+            password_error = password_error + "\nThe password must contain at least 2 numbers"
+        # password has not errored yet
+        else:
+            password_validation, password_error = False, "The password must contain at least 2 numbers"
+    return password_validation, password_error
 
 def test_password_validator():
     '''
@@ -61,22 +75,35 @@ def test_password_validator():
     Returns:
               None
     '''     
-    # TEST 1: Testing if password_validator recognizes string length with a success check and failure check
-    print("TEST 1: Testing if password_validator recognizes string length with a success check and failure check:")    
-        # password fails validation
-    print("'hi' should validate False and report it is to short:")
-    password_validation, password_error = password_Validator("hi")
-    if (password_validation == False and password_error == "Password must be at least 8 characters"):
-        print("Pass!")
-    else:
-        print("Fail!")
-        # password succeeds validation
-    password_validation, password_error = password_Validator("strongpassword")
-    print("'strongpassword' should validate True and report no error:")
-    if (password_validation == True and password_error == "None"):
+    # TEST 1: Testing if password_validator recognizes string length and number of digits in the password with a success check
+    print("TEST 1: Testing if password_validator recognizes string length and number of digits in the password with a success check:")
+    # password succeeds validation
+    password_validation, password_error = password_Validator("strongpassword12")
+    print("'strongpassword12' should validate True and report no error:")
+    if (password_validation == True and password_error == None):
         print("Pass!")
     else:
         print("Fail!")    
+        
+    # TEST 2: Testing if password_validator recognizes the password does not have at least 2 digits
+    print("TEST 2: Testing if password_validator recognizes if the password has at least 2 digits with a success and failure check:")    
+        # password fails validation
+    print("'strongpassword' should validate False and report it does not have at least 2 numbers:")
+    password_validation, password_error = password_Validator("strongpassword")
+    if (password_validation == False and password_error == "The password must contain at least 2 numbers"):
+        print("Pass!")
+    else:
+        print("Fail!")
+    
+    # TEST 3: Testing if password_validator recognizes a failure in both number of characters and digits
+    print("TEST 3: Testing if password_validator recognizes a failure in both number of characters and digits:")    
+        # password fails validation
+    print("'hi' should validate False and report it does not have at least 8 characters and 2 numbers:")
+    password_validation, password_error = password_Validator("hi")
+    if (password_validation == False and password_error == "Password must be at least 8 characters\nThe password must contain at least 2 numbers"):
+        print("Pass!")
+    else:
+        print("Fail!")       
     
     
 test_password_validator()
